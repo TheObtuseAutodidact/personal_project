d28ddaae24423c9e771ed5ad4d47f0e16562b062
Obtuse <theobtuseautodidact@gmail.com>
2016-04-20 13:07:47 -0600

    add api endpoint for locations
    added resouces and controllers as well as responders gem
    added location model

--- the_hotness/config/routes.rb
+++ the_hotness/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  namespace :api do
+    namespace :v1 do
+      resources :locations, only: [:index, :create], defaults: { format: 'json' }
+    end
+  end
 
-  resouces :contacts, only: [:create]
   # You can have the root of your site routed with "root"
   # root "landing#landing" landing page will probably be on phonegap
   # root 'welcome#index'
